d10057f0605039dcf544fc452f1152767215c820
Leonardo Dias <pauloleonardo_dp@outlook.com>
2020-05-18 22:38:28 -0300

    Aplicação com Home e Gêneros linkados. Dados conectados com api series-server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import axios from 'axios';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const Home = () => {
+  return <h1> Home</h1>
+}
+const Generos = () => {
+  return <h1> Gêneros</h1>
+}
 
 function App() {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    axios.get('/api').then( res => {
+      setData(res.data)
+    })
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+          <Header />
+          <Route path='/' exact component={Home} />
+          <Route path='/generos' component={Generos} />
+          <pre> {JSON.stringify(data)}</pre>
+        </div>
+      </Router>
     </div>
   );
 }
