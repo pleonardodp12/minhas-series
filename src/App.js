1b487de3fc88886b996487c03882bac051c15660
Leonardo Dias <pauloleonardo_dp@outlook.com>
2020-05-19 01:19:18 -0300

    Rotas Generos, com botões de Editar e Remover Gênero funcionando

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import Header from './Header';
+import Generos from './Generos';
+import NovoGenero from './NovoGenero';
 import axios from 'axios';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import EditarGenero from './EditarGenero'
 
 const Home = () => {
   return <h1> Home</h1>
 }
-const Generos = () => {
-  return <h1> Gêneros</h1>
-}
+
 
 function App() {
   const [data, setData] = useState({})
@@ -23,7 +24,9 @@ function App() {
         <div>
           <Header />
           <Route path='/' exact component={Home} />
-          <Route path='/generos' component={Generos} />
+          <Route path='/generos' exact component={Generos} />
+          <Route path='/generos/novo' exact component={NovoGenero} />
+          <Route path='/generos/:id' exact component={EditarGenero} />
           <pre> {JSON.stringify(data)}</pre>
         </div>
       </Router>
